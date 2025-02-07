df15163502ea0764099decc9f50a2fed38260bc8
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-02-07 21:43:43 +1100

    Use disjointed graph, links for leader lines

---
+++ data/labels.ts
@@ -0,0 +1,42 @@
+export const WIDTH = 1000;
+export const HEIGHT = 750;
+
+const CHAR_WIDTH = 20;
+const LINE_HEIGHT = 20;
+export const labels = [
+  {
+    id: "HSS Column",
+    x: WIDTH * 0.3,
+    y: HEIGHT * 0.28,
+    width: CHAR_WIDTH * 10,
+    height: LINE_HEIGHT,
+  },
+  {
+    id: "Through Plate",
+    x: WIDTH * 0.34,
+    y: HEIGHT * 0.38,
+    width: CHAR_WIDTH * 13,
+    height: LINE_HEIGHT,
+  },
+  {
+    id: "Beam",
+    x: WIDTH * 0.5,
+    y: HEIGHT * 0.32,
+    width: CHAR_WIDTH * 5,
+    height: LINE_HEIGHT,
+  },
+  {
+    id: "16 mm Ø Bolt",
+    x: WIDTH * 0.36,
+    y: HEIGHT * 0.467,
+    width: CHAR_WIDTH * 12,
+    height: LINE_HEIGHT,
+  },
+  {
+    id: "12mm flange",
+    x: WIDTH * 0.6,
+    y: HEIGHT * 0.32,
+    width: CHAR_WIDTH * 5,
+    height: LINE_HEIGHT,
+  },
+];
--- script.ts
+++ script.ts
@@ -1,91 +1,82 @@
 import { createCanvas, loadImage } from "canvas";
 // @ts-ignore
 import { writeFile } from "fs/promises";
-import { forceSimulation, forceX, forceY, forceCollide } from "d3-force";
-import rectangleForceCollide from "./rectangleForceCollide.ts";
+import { forceSimulation, forceManyBody, forceLink } from "d3-force";
 import forceBoundary from "d3-force-boundary";
-const WIDTH = 1000;
-const HEIGHT = 750;
 
-const CHAR_WIDTH = 20;
-const LINE_HEIGHT = 20;
+// Utils & data
+import rectangleForceCollide from "./rectangleForceCollide.ts";
+import { labels, WIDTH, HEIGHT } from "./data/labels.ts";
 
-const labels = [
-  {
-    id: "HSS Column",
-    x: WIDTH * 0.3,
-    y: HEIGHT * 0.28,
-    width: CHAR_WIDTH * 10,
-    height: LINE_HEIGHT,
-  },
-  {
-    id: "Through Plate",
-    x: WIDTH * 0.34,
-    y: HEIGHT * 0.38,
-    width: CHAR_WIDTH * 13,
-    height: LINE_HEIGHT,
-  },
-  {
-    id: "Beam",
-    x: WIDTH * 0.5,
-    y: HEIGHT * 0.32,
-    width: CHAR_WIDTH * 5,
-    height: LINE_HEIGHT,
-  },
-  {
-    id: "16 mm Ø Bolt",
-    x: WIDTH * 0.36,
-    y: HEIGHT * 0.467,
-    width: CHAR_WIDTH * 12,
-    height: LINE_HEIGHT,
-  },
-  {
-    id: "1623 mm Ø Bolt",
-    x: WIDTH * 0.336,
-    y: HEIGHT * 0.4367,
-    width: CHAR_WIDTH * 12,
-    height: LINE_HEIGHT,
-  },
-];
-
-const originalPositions = labels.map((d) => ({ id: d.id, x: d.x, y: d.y }));
 const bbox = {
   x0: WIDTH * 0.35,
   y0: HEIGHT * 0.1,
   x1: WIDTH * 0.8,
   y1: HEIGHT * 0.25,
-  get cx() {
-    return (this.x0 + this.x1) / 2;
-  },
-  get cy() {
-    return (this.y0 + this.y1) / 2;
-  },
 };
 
-const simulation = forceSimulation(labels)
-  // https://observablehq.com/@john-guerra/d3-force-boundary
-  .force("boundary", forceBoundary(bbox.x0, bbox.y0, bbox.x1, bbox.y1))
-  // // https://observablehq.com/@roblallier/rectangle-collision-force
-  .force("collide", rectangleForceCollide())
-  // TODO: Links within disjointed graphs should not cross over
+// Create nodes and links
+const nodes: any = [];
+const links: any = [];
+
+labels.forEach((label, index) => {
+  const originalNode = {
+    pos: "start",
+    id: `${label.id}-original`,
+    x: label.x,
+    y: label.y,
+    // Lock the position of the original node
+    // In future if there's a degree of movement allowed
+    // i.e. column can be labelled anywhere along the column face
+    // we can unconstrain one of these
+    fx: label.x,
+    fy: label.y,
+  };
+
+  const endNode = {
+    pos: "end",
+    id: `${label.id}`,
+    x: label.x,
+    y: label.y,
+    width: label.width,
+    height: label.height,
+  };
+  const halfwayNode = {
+    pos: "halfway",
+    id: `${label.id}-halfway`,
+    x: (label.x + (bbox.x0 + bbox.x1) / 2) / 2,
+    y: (label.y + (bbox.y0 + bbox.y1) / 2) / 2,
+  };
+
+  nodes.push(originalNode, halfwayNode, endNode);
+  links.push(
+    { source: originalNode.id, target: halfwayNode.id },
+    { source: halfwayNode.id, target: endNode.id }
+  );
+});
+
+const simulation = forceSimulation(nodes)
+  // multi-leader line should be attracted to each other
+  .force(
+    "link",
+    forceLink(links).id((d) => d.id)
+  )
+  // Keeps each annotation line away from each other
+  // Increase the strength of this so its always maintained
   // https://observablehq.com/@d3/disjoint-force-directed-graph/2
+  .force("charge", forceManyBody().strength(-1000))
+  .force("collide", rectangleForceCollide())
+  // https://observablehq.com/@roblallier/rectangle-collision-force
+  .force("boundary", forceBoundary(bbox.x0, bbox.y0, bbox.x1, bbox.y1))
   .stop();
 
-simulation.tick(30);
-
-const positionsComparison = originalPositions.map((original, index) => ({
-  id: original.id,
-  originalX: original.x,
-  originalY: original.y,
-  finalX: labels[index].x,
-  finalY: labels[index].y,
-}));
+simulation.tick(50);
 
 // RENDER
 const canvas = createCanvas(WIDTH, HEIGHT);
 const ctx = canvas.getContext("2d");
 
-const image = await loadImage("diagram_alpha.png");
+const image = await loadImage("assets/blank_img.png");
 ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
@@ -95,39 +86,39 @@ ctx.lineWidth = 2;
 
 ctx.rect(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
 ctx.stroke();
-
-positionsComparison.forEach((d, idx) => {
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 3;
+nodes.forEach((node) => {
+  ctx.fillStyle = node.fixed ? "red" : "blue";
   ctx.beginPath();
-  ctx.moveTo(d.originalX, d.originalY);
-  ctx.lineTo(d.finalX, d.finalY);
-  ctx.stroke();
-
-  // Final positions (Red)
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(d.finalX, d.finalY, 5, 0, Math.PI * 2);
+  ctx.arc(node.x, node.y, 5, 0, Math.PI * 2);
   ctx.fill();
 
-  // Draw the label text
-  ctx.font = "24px Arial";
-  ctx.textBaseline = "bottom";
-  ctx.fillText(d.id, d.finalX, d.finalY);
-
-  // Draw rectangle with x and y starting from bottom left
-  const label = labels.find((label) => label.id === d.id);
-  if (label) {
+  if (node.pos === "end") {
+    // Draw rectangle with x and y starting from bottom left
     ctx.strokeStyle = "green";
     ctx.lineWidth = 2;
-    ctx.strokeRect(
-      d.finalX,
-      d.finalY - label.height,
-      label.width,
-      label.height
-    );
+    ctx.strokeRect(node.x, node.y - node.height, node.width, node.height);
+
+    // Final positions (Red)
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Draw the label text
+    ctx.font = "24px Arial";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(node.id, node.x, node.y);
   }
 });
 
+links.forEach((link) => {
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.moveTo(link.source.x, link.source.y);
+  ctx.lineTo(link.target.x, link.target.y);
+  ctx.stroke();
+});
+
 const buffer = canvas.toBuffer("image/png");
-await writeFile(new URL("./output.png", import.meta.url), buffer);
+await writeFile(new URL("./out/output.png", import.meta.url), buffer);
